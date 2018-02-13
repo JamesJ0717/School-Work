b2a9eeff882095a7d1d4f3b9c4ff5ce8964b2d01
James Johnson <TripleJ547@users.noreply.github.com>
2018-02-13 12:34:40 -0500

    Homework

---
+++ 249Java/Assign03_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Assign03_3 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		do{
+			System.out.println("Please enter a string.");
+			string userInput = input.nextLine();
+
+			string outputString = "";
+
+			for (char: userInput) {
+				
+				
+			}
+
+		}
+		while (userInput != "NULL");
+
+	}
+}
