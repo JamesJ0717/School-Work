15a68667b4c71552d72c48501ceb199efa06e113
James Johnson <TripleJ547@users.noreply.github.com>
2018-02-20 21:47:13 -0500

    Update Assign03_3.java

--- 249Java/Assign03_3.java
+++ 249Java/Assign03_3.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Assign03_3 {
@@ -30,35 +29,4 @@ public class Assign03_3 {
 		while (userInput != "");
 
 	}
-=======
-import java.util.Scanner;
-
-public class Assign03_3 {
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		String userInput = "";
-
-		do{
-			System.out.println("Please enter a string.");
-			userInput = input.nextLine();
-			char[] userArray = userInput.toCharArray();
-			String outputString = "";
-
-			for (char letter: userArray) {
-				if (Character.isDigit(letter)){
-					outputString += "_";
-				}
-				else{
-					outputString += letter;
-				}
-				
-			}
-			System.out.println(outputString);
-
-		}
-		while (!userInput.isEmpty());
-
-	}
->>>>>>> 95b6c9a335ab7163c7d1f1c06235a8d6a4ec7efb
 }
