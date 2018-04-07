779be6d43e73e405698721c17c1e62afa10ea2ff
James Johnson <TripleJ547@users.noreply.github.com>
2018-04-06 23:54:16 -0400

    assignment 06

---
+++ 249Java/Assign06/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+  public static double eval(String expr) throws InvalidExpressionException {
+    //TODO finish code
+    try{
+
+    }
+    catch (Exception){
+
+    }
+    return (0.0);
+  }
+
+}
