31b8eb89336e9d705f081ff2553fb732a7ccca12
triplej547 <tripl3j547@gmail.com>
2018-04-10 16:38:02 -0400

    homework

--- 249Java/Assign06/WordCountData.java
+++ 249Java/Assign06/WordCountData.java
@@ -1,23 +1,23 @@
 public class WordCountData {
-    private int charCnt;
-    private int wordCnt;
-    private int lineCnt;
+  private int charCnt;
+  private int wordCnt;
+  private int lineCnt;
 
-    public WordCountData(int numChars, int numWords, int numLines) {
-        this.charCnt = numChars;
-        this.wordCnt = numWords;
-        this.lineCnt = numLines;
-    }
+  public WordCountData(int numChars, int numWords, int numLines) {
+    this.charCnt = numChars;
+    this.wordCnt = numWords;
+    this.lineCnt = numLines;
+  }
 
-    public int getCharCnt() {
-        return charCnt;
-    }
+  public int getCharCnt() {
+    return charCnt;
+  }
 
-    public int getWordCnt() {
-        return wordCnt;
-    }
+  public int getWordCnt() {
+    return wordCnt;
+  }
 
-    public int getLineCnt() {
-        return lineCnt;
-    }
+  public int getLineCnt() {
+    return lineCnt;
+  }
 }
--- 249Java/Assign06/WordCounter.java
+++ 249Java/Assign06/WordCounter.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 import java.net.URL;
 
 public class WordCounter {
-    public static WordCountData count(String path) throws Exception{
-        if(path.contains("http")){
-            try {
-                Scanner pageInput = new Scanner(url.openStream());
+  public static WordCountData count(String path) throws Exception{
+    if(path.contains("http")){
+      try {
+        Scanner pageInput = new Scanner(url.openStream());
 
-                int wordCnt = 0;
-                int lineCnt = 0;
-                int charCnt = 0;
+        int wordCnt = 0;
+        int lineCnt = 0;
+        int charCnt = 0;
 
-                while(pageInput.hasNext()){
-                    if (pageInput.next() == " ") {
-                        wordCnt++;
-                    }
-                    else if (pageInput.hasNext("\n")) {
-                        lineCnt++;
-                    }
-                }
-            }
-            catch (Exception e) {
-
-            }
-
-
+        while(pageInput.hasNext()){
+          if (pageInput.next() == " ") {
+            wordCnt++;
+          }
+          else if (pageInput.hasNext("\n")) {
+            lineCnt++;
+          }
         }
-        return ();
+      }
+      catch (Exception e) {
+
+      }
+
+
     }
+    return ();
+  }
 }
