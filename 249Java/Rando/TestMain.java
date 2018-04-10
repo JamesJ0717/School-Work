34f8dc4fac759bf75e82b1e9a77e5221154e1af0
james <tripl3j547@gmail.com>
2018-04-10 19:43:30 -0400

    wahh

--- 249Java/Rando/TestMain.java
+++ 249Java/Rando/TestMain.java
@@ -8,8 +8,16 @@
  *
  * @author Jimmy
  */
+import java.util.Scanner;
+
 public class TestMain {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("War... war never changes.");
-    }    
+
+        Scanner input = new Scanner(System.in);
+
+        String word = input.nextLine();
+        word.toLowerCase();
+        System.out.println(word);
+    }
 }
