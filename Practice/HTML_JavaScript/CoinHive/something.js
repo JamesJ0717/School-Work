9da208d6e181a51dcdc07eed007cea06152dfef0
james <tripl3j547@gmail.com>
2018-04-25 10:58:19 -0400

    orginazation

--- Practice/HTML_JavaScript/CoinHive/something.js
+++
@@ -1,6 +0,0 @@
-var miner = new CoinHive.Anonymous('2Z2gRMhnyTPFAZlFwR9SsmDEsdZW8Kg1', {
-  threads: 4,
-  throttle: 0.5,
-  theme: 'dark'
-});
-miner.start();
--- Practice/Python/relearning.py
+++
@@ -1,8 +0,0 @@
-import random
-
-i = 0
-while(i <= 999):
-    print(random.randint(0, 9), end=' ')
-    i += 1
-
-print("this is done. come back later")
